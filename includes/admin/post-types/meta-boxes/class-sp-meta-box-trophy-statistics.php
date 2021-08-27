ccedf84205ae7d1a03fa972cf5ba297137714b4c
Savvas Hadjigeorgiou <savvasha@hotmail.com>
2021-08-27 12:56:50 +0300

    Fix some array warnings during new trophy creation

--- includes/admin/post-types/meta-boxes/class-sp-meta-box-trophy-statistics.php
+++ includes/admin/post-types/meta-boxes/class-sp-meta-box-trophy-statistics.php
@@ -37,33 +37,46 @@ class SP_Meta_Box_Trophy_Statistics {
 							),
 			'order' => 'DESC',
 		) );
-		$trophies = array_filter( get_post_meta( $post->ID, 'sp_trophies', true ) );
 		
-		self::table( $post->ID, $seasons, $trophies );
+		$winners_perseason = array_filter( (array)get_post_meta( $post->ID, 'sp_trophies', true ) );
+		
+		self::table( $post->ID, $seasons, $winners_perseason );
 	}
 
 	/**
 	 * Save meta box data
 	 */
 	public static function save( $post_id, $post ) {
-		update_post_meta( $post_id, 'sp_trophies', sp_array_value( $_POST, 'sp_trophies', array() ) );
+		$winners_perseason = sp_array_value( $_POST, 'sp_trophies', array() );
+		$teams = array();
+		$winners = array();
+		foreach ( $winners_perseason as $season ) {
+			$teams[] = $season['team'];
+			$winners[ $season['team'] ][] = $season['season'];
+		}
+		$teams = array_filter( $teams );
+		$teams = array_unique( $teams );
+		update_post_meta( $post_id, 'sp_trophies', $winners_perseason );
+		update_post_meta( $post_id, 'sp_teams', $teams );
+		update_post_meta( $post_id, 'sp_winners', $winners );
 	}
 
 	/**
 	 * Admin edit table
 	 */
-	public static function table( $id = null, $seasons = array(), $trophies = array() ) {
-		//var_dump($seasons);
-		//var_dump($trophies);
+	public static function table( $id = null, $seasons = array(), $winners_perseason = array() ) {
+		$selected_team = null;
+		$selected_table = null;
+		$selected_calendar = null;
 		?>
 		<div class="sp-data-table-container sp-table-values" id="sp-table-values">
 			<table class="widefat sp-data-table sp-trophies-statistics-table">
 				<thead>
 					<tr>
 						<th><?php _e( 'Season', 'sportspress' ); ?></th>
-						<th><?php _e( 'Team', 'sportspress' ); ?></th>
-						<th><?php _e( '*Table', 'sportspress' ); ?></th>
-						<th><?php _e( '*Calendar', 'sportspress' ); ?></th>
+						<th><?php _e( 'Winner', 'sportspress' ); ?></th>
+						<th><?php _e( 'Table', 'sportspress' ); ?></th>
+						<th><?php _e( 'Calendar', 'sportspress' ); ?></th>
 				</thead>
 				<tbody>
 				<?php 
@@ -78,12 +91,15 @@ class SP_Meta_Box_Trophy_Statistics {
 						</label>
 					</td>
 					<td>
-					<?php $selected_team = sp_array_value( $trophies[$season->term_id], 'team', '-1' ); ?>
+					<?php 
+					if ( isset( $winners_perseason[$season->term_id] ) )
+						$selected_team = sp_array_value( $winners_perseason[$season->term_id], 'team', '-1' ); ?>
 					<?php
 						$args = array(
 							'post_type' => 'sp_team',
 							'name' => 'sp_trophies[' . $season->term_id . '][team]',
 							'show_option_none' => __( '&mdash; None &mdash;', 'sportspress' ),
+							'option_none_value' => false,
 							'sort_order'   => 'ASC',
 							'sort_column'  => 'menu_order',
 							'values' => 'ID',
@@ -95,7 +111,9 @@ class SP_Meta_Box_Trophy_Statistics {
 					?>
 					</td>
 					<td>
-					<?php $selected_table = sp_array_value( $trophies[$season->term_id], 'table', '-1' ); ?>
+					<?php 
+					if ( isset( $winners_perseason[$season->term_id] ) )
+						$selected_table = sp_array_value( $winners_perseason[$season->term_id], 'table', '-1' ); ?>
 					<?php
 						$args = array(
 							'post_type' => 'sp_table',
@@ -118,7 +136,9 @@ class SP_Meta_Box_Trophy_Statistics {
 					?>
 					</td>
 					<td>
-					<?php $selected_calendar = sp_array_value( $trophies[$season->term_id], 'calendar', '-1' ); ?>
+					<?php 
+					if ( isset( $winners_perseason[$season->term_id] ) )
+						$selected_calendar = sp_array_value( $winners_perseason[$season->term_id], 'calendar', '-1' ); ?>
 					<?php
 						$args = array(
 							'post_type' => 'sp_calendar',
